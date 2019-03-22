d19ed1f8feda3027c02c7519a05d3f57def4c67f
Thinh <thinh@Thinhs-MacBook-Pro.local>
2019-03-21 20:16:36 -0700

    fixed

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { CardComponent } from './body/card/card.component';
+import { SidebarComponent } from './body/sidebar/sidebar.component';
+import { ContentComponent } from './body/content/content.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BodyComponent,
+    CardComponent,
+    SidebarComponent,
+    ContentComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
